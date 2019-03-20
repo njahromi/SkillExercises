7ea3f3224de8f8b4d309bf7259601d3307f3d82f
njahromi <44483465+njahromi@users.noreply.github.com>
2019-03-20 10:34:17 -0700

    Update Solution.java

--- SockMerchant/src/main/Solution.java
+++ SockMerchant/src/main/Solution.java
@@ -1,43 +1,34 @@
 package main;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.HashMap;
 
 public class Solution {
 
-	// Complete the sockMerchant function below.
-	static int sockMerchant(int n, int[] ar) {
-		return n;
+    private static Scanner in;
 
-	}
-
-	private static final Scanner scanner = new Scanner(System.in);
-
-	public static void main(String[] args) throws IOException {
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-		int n = scanner.nextInt();
-		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-		int[] ar = new int[n];
-
-		String[] arItems = scanner.nextLine().split(" ");
-		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-		for (int i = 0; i < n; i++) {
-			int arItem = Integer.parseInt(arItems[i]);
-			ar[i] = arItem;
-		}
-
-		int result = sockMerchant(n, ar);
-
-		bufferedWriter.write(String.valueOf(result));
-		bufferedWriter.newLine();
-
-		bufferedWriter.close();
-
-		scanner.close();
-	}
+	public static void main(String[] args) {
+        in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        HashMap<Integer, Integer> inventory = new HashMap<Integer,Integer>();
+        
+        int matchingPairs = 0;
+        
+        for(int i=0; i < n; i++)
+        {
+            int color = in.nextInt();
+            
+            //This checks if we already have 1 sock of that color and if we do then we increment matchingPairs and set unmatched      
+            //socks of that color back to 0
+            if(inventory.containsKey(color) && inventory.get(color).equals(new Integer(1)))
+            {
+                inventory.put(color,0);
+                matchingPairs++;
+                continue;
+            }
+            inventory.put(color,1);
+        }
+        System.out.println(matchingPairs);
+    }
 }
