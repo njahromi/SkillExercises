267006076bb80784a557a453fc45148e01c9d77c
njahromi <44483465+njahromi@users.noreply.github.com>
2019-03-21 15:52:50 -0700

    Updated the solution for Jumping on Clouds

--- JumpingOnClouds/src/main/Solution.java
+++ JumpingOnClouds/src/main/Solution.java
@@ -1,47 +1,34 @@
 package main;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-    // Complete the jumpingOnClouds function below.
-    static int jumpingOnClouds(int[] c) {
-		return 0;
+		int n = sc.nextInt();
+		int[] c = new int[n];
+		for (int i = 0; i < c.length; i++) {
+			c[i] = sc.nextInt();
+		}
 
+		int[] minSteps = new int[c.length];
+		Arrays.fill(minSteps, 1, minSteps.length, -1);
+		for (int i = 0; i < minSteps.length; i++) {
+			if (c[i] == 1) {
+				continue;
+			}
 
-    }
+			if (i >= 1 && c[i - 1] == 0 && minSteps[i - 1] >= 0) {
+				minSteps[i] = Math.min((minSteps[i] < 0) ? Integer.MAX_VALUE : minSteps[i], minSteps[i - 1] + 1);
+			}
+			if (i >= 2 && c[i - 2] == 0 && minSteps[i - 2] >= 0) {
+				minSteps[i] = Math.min((minSteps[i] < 0) ? Integer.MAX_VALUE : minSteps[i], minSteps[i - 2] + 1);
+			}
+		}
+		System.out.println(minSteps[minSteps.length - 1]);
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] c = new int[n];
-
-        String[] cItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int cItem = Integer.parseInt(cItems[i]);
-            c[i] = cItem;
-        }
-
-        int result = jumpingOnClouds(c);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
+		sc.close();
+	}
 }
