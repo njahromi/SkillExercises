c7510fec202030710b2dad5fcb71f8bc057f0bcf
BuildTools <unconfigured@null.spigotmc.org>
2019-05-14 17:54:40 -0700

    Practice with Strings

---
+++ JavaStrings/src/main/Assignment1.java
@@ -0,0 +1,19 @@
+package main;
+
+/*
+ * Write a class named Assignment1 that declares two strings, “Hello” and (your name) and concatenates
+them together. Print the result.
+ */
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+
+		String a = "Hello, ";
+		String b = "Nate.";
+
+		System.out.println(a + b);
+
+	}
+
+}
