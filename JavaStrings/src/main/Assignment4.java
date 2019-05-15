b42dbaa6c7c72d7503f44b204545bfa36fab46c0
BuildTools <unconfigured@null.spigotmc.org>
2019-05-14 18:03:27 -0700

    Worked on two additional exercises

---
+++ JavaStrings/src/main/Assignment4.java
@@ -0,0 +1,20 @@
+package main;
+
+/*
+ * Write a class named Assignment4 that declares a string "abcdefghijklmnopqrstuvwxyz". Use the
+indexOf() method to print the index of "s" and "f".
+
+ */
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		
+		System.out.println(alphabet.indexOf("s"));
+		System.out.println(alphabet.indexOf("f"));
+
+	}
+
+}
---
+++ JavaStrings/src/main/Assignment5.java
@@ -0,0 +1,18 @@
+package main;
+
+/*
+ * Write a class named Assignment5 that uses the substring() method to print the last 5 characters of
+"Hello My Name is Java".
+ */
+
+public class Assignment5 {
+	
+	public static void main (String[]args) {
+		
+		String a = "Hello My Name is Java";
+		
+		System.out.println(a.substring(15));
+		
+	}
+
+}
