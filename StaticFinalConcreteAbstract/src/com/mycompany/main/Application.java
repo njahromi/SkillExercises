40597db993747ea7293793722eff3c28b225c6c1
BuildTools <unconfigured@null.spigotmc.org>
2019-05-19 21:56:27 -0700

    Added Project Descriptions

--- StaticFinalConcreteAbstract/src/com/mycompany/main/Application.java
+++ StaticFinalConcreteAbstract/src/com/mycompany/main/Application.java
@@ -7,6 +7,19 @@ numbers. Use this method in an Application class, to compute the sum of two numb
 your static method.
  */
 
+/*
+ * Create a class named FinalClass and define an instance variable, myConstant. Use the final keyword
+to mark the instance variable as final. This makes it a constant. Add a statement to your Application
+class to print the value of this constant.
+ */
+
+/*
+ * Write a class named AbstractClass and declare it abstract. It should have an abstract method run().
+Declare another class ConcreteClass and it should extend AbstractClass. Implement the run() method.
+In the Application class, add a statement to create an instance of ConcreteClass and then call the run()
+method.
+ */
+
 public class Application {
 
 	public static void main(String[] args) {
